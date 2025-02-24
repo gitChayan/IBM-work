f403dd90978965044b4796a2ab7d84e4d6fa61d5
gitChayan <sankichayanpc@gmail.com>
2025-02-24 13:27:48 +0530

    Day9

---
+++ Day9/Assignment/Assignment_js.js
@@ -0,0 +1,55 @@
+// let x = "5"; let y = 5;
+//  console.log(x == y); 
+//   console.log(x === y); 
+
+// function testScope() { 
+//     if (true) { 
+//         var a = 10;
+//         let b = 20; 
+//         const c = 30; 
+//     } 
+//     console.log(a); 
+//     // console.log(b); 
+//     // console.log(c); 
+// } 
+// testScope();
+// const name = "John"; 
+// function greet() { 
+//     if (true) { 
+//         const message = "Hello " + name; 
+//     } 
+//     console.log(message); 
+// } 
+// greet();
+// const obj = { 
+//     name: "Alice", 
+//     sayHello: function() { 
+//         setTimeout(() => { 
+//             console.log("Hello, " + this.name); 
+//         }, 1000); 
+//     } 
+// }; 
+// obj.sayHello();
+ 
+// console.log(a); 
+// var a = 1️0; 
+// console.log(b); 
+// let b = 2️0;
+
+var double = function(n) { 
+    
+    return n * 2️; 
+    console.log(double(4️));
+    
+};
+function square(n) { 
+    return n * n; 
+     
+    
+} 
+
+console.log(square(5));
+ 
+
+ 
+
