5fa33d9a24ce0440f9254d1f1ae5e5ab5d21e86b
gitChayan <sankichayanpc@gmail.com>
2025-03-03 13:55:54 +0530

    Day14

---
+++ Day14/Assignment/cart.js
@@ -0,0 +1,42 @@
+function getData(){
+    let parsedData=JSON.parse(localStorage.getItem("cartData"));
+    // let container = document.getElementById('cart-items');
+    // let emptyMsg = document.getElementById('empty-cart');
+    
+    console.log(parsedData);
+   
+    parsedData.forEach((el,index)=>{
+        let productBox=document.createElement("div");//make a big box
+        productBox.className="product-box";//applying css 
+        let heading = document.createElement("h3");
+        heading.innerText=el.category;
+    
+        let img=document.createElement("img");
+        img.src=el.image;
+    
+        let name = document.createElement("p");
+        name.innerText=el.name;
+    
+        let price=document.createElement("p");
+        price.innerText=el.price;
+    
+        let rating=document.createElement("p");
+        rating.innerText=el.rating;
+         
+        let button=document.createElement("button");
+        button.innerText="Purchase";
+        button.addEventListener("click",()=>{
+            purchase(el,index);
+
+        });
+
+        productBox.append(heading,img,name,price,rating,button);
+
+        document.getElementById("cart-items").append(productBox);
+    
+    
+    });
+}
+getData();
+
+
---
+++ Day14/classwork/d13.js
@@ -0,0 +1,73 @@
+// // foreach
+// let arr=[1,2,3,4,5,6,6,7,8,"mc","bc"];
+// arr.forEach((element,index) => {
+//    console.log(element,index); 
+// });
+// let ans= arr.forEach((el,i)=>{
+//             return el;
+// });   
+// console.log(ans) ;//this return undefined whereas map return arry
+
+// let ans= arr.map((el,i)=>{
+//             //  return el;
+//              return el**4;// this is task perform by 
+//      }); 
+//      console.log(ans);
+
+//filter
+// let output= arr.filter((el,i)=>{   //filter out on condition and return a new array
+//     // return el%2==0 && el!=6;
+//     return typeof el=="string" || el%5==0;
+// });
+// console.log(output);
+
+//reduce
+// let output=arr.reduce((ace,el)=>{
+//     return ace+el;
+// },1); //accumulator   "When accumulator not defined then first elemet of array assign as accumulator"
+// console.log(output);
+
+
+// let output= arr.filter((el,i)=>{   
+//      return el%2==0 ;
+        
+//      }).map((el,index)=>{
+//         return el*2;
+//      }).reduce((ace,el)=>{
+//         return ace+el;
+
+//      });
+//      console.log(output);
+
+// let a=[123,35,4,6,2323,65,74,4,7];
+// let anss=a.sort((a,b)=>{
+//     return a-b;
+// });
+// console.log(anss);
+    
+// let data =[
+//     {name:"watch",price:22200,des:"poor",rating:4},
+//     {name:"watch1",price:1200,des:"good",rating:4},
+//     {name:"watch1",price:2100,des:"good",rating:4},
+//     {name:"watch1",price:21100,des:" good good",rating:4},
+//     {name:"watch1",price:211100,des:" very good",rating:4}
+// ]
+// data.sort((a,b)=>{
+//    return a.price-b.price;
+// })
+// console.log(data);
+let arr=[3,6,9,2,56,34,98,56,23,12,90];
+function sorting(arr) {
+    for(let i=0;i<=arr.length;i++){
+        for(let j=0;j<=arr.length;j++){
+            if(arr[j]>arr[j+1]){
+                let a=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=a;
+            }
+        }
+    }
+    return arr;
+    
+}
+console.log(sorting(arr));
