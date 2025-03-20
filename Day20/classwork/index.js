7103ace28ecb8ff6d32107b3378d5499b9f1445c
gitChayan <sankichayanpc@gmail.com>
2025-03-20 21:04:25 +0530

    Day20 updated

---
+++ Day20/classwork/index.js
@@ -0,0 +1,10 @@
+const os =require("os");
+// console.log(os.homedir());
+// console.log(os.machine());
+// console.log(os.cpus());
+// console.log(os.freemem());
+const http=require("http");
+
+console.log(http.METHODS);
+
+console.table([{ a: 1, b: 'Y' }, { a: 'Z', b: 2 }]);
